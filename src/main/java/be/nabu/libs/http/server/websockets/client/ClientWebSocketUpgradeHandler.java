cc16fb1a789e0ee9b5c986786257145c28652d82
Alexander Verbruggen <alex@nabu.be>
2020-01-15 09:59:23 +0100

    allow adding of proxy path

--- src/main/java/be/nabu/libs/http/server/websockets/client/ClientWebSocketUpgradeHandler.java
+++ src/main/java/be/nabu/libs/http/server/websockets/client/ClientWebSocketUpgradeHandler.java
@@ -1,6 +1,7 @@
 package be.nabu.libs.http.server.websockets.client;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +21,7 @@ import be.nabu.libs.http.api.LinkableHTTPResponse;
 import be.nabu.libs.http.api.server.AuthenticationHeader;
 import be.nabu.libs.http.api.server.MessageDataProvider;
 import be.nabu.libs.http.core.HTTPUtils;
+import be.nabu.libs.http.core.ServerHeader;
 import be.nabu.libs.http.server.websockets.WebSocketHandshakeHandler;
 import be.nabu.libs.http.server.websockets.api.OpCode;
 import be.nabu.libs.http.server.websockets.api.WebSocketMessage;
@@ -33,6 +35,7 @@ import be.nabu.libs.nio.api.KeepAliveDecider;
 import be.nabu.libs.nio.api.MessagePipeline;
 import be.nabu.libs.nio.api.Pipeline;
 import be.nabu.libs.nio.api.UpgradeableMessagePipeline;
+import be.nabu.libs.resources.URIUtils;
 import be.nabu.utils.mime.api.Header;
 import be.nabu.utils.mime.impl.MimeUtils;
 
@@ -99,8 +102,30 @@ public class ClientWebSocketUpgradeHandler implements EventHandler<HTTPResponse,
 				AuthenticationHeader authenticationHeader = HTTPUtils.getAuthenticationHeader(request.getContent().getHeaders());
 				Token token = authenticationHeader == null ? null : authenticationHeader.getToken();
 				
+				String requestPath = request.getTarget();
+				
+				// currently the only situation where we don't want to mask responses is in the reverse proxy which has to forward stuff
+				// it is also in this context that we do proxy path rewriting, so we reuse the same boolean
+				// if at some point we want multiple scenarios: add an additional boolean!
+				if (!shouldMaskResponses) {
+					// if it is an absolute path, we want to actual path (do we need query params etc as well?)
+					// this may be more broadly applicable, but for now it is only breaking the reverse proxy so we will localize the fix
+					if (requestPath.matches("^[\\w]+:/.*")) {
+						URI uri = new URI(URIUtils.encodeURI(requestPath, false));
+						requestPath = uri.getPath();
+						if (uri.getQuery() != null) {
+							requestPath += "?" + uri.getQuery();
+						}
+					}
+					// the request that is linked to the response may already have been rewritten to strip the proxy path
+					// if however, we leave the path like that, we will not be able to correctly subscribe to any messages because the paths don't match
+					Header proxyPath = MimeUtils.getHeader(ServerHeader.PROXY_PATH.getName(), request.getContent().getHeaders());
+					if (proxyPath != null && proxyPath.getValue() != null) {
+						requestPath = proxyPath.getValue().replaceAll("[/]+$", "") + "/" + requestPath.replaceFirst("^[/]+", "");
+					}
+				}
 				MessagePipeline<WebSocketRequest, WebSocketMessage> upgrade = ((UpgradeableMessagePipeline<?, ?>) pipeline).upgrade(
-					new WebSocketRequestParserFactory(dataProvider, protocols, request.getTarget(), version, token, device, tokenValidator), 
+					new WebSocketRequestParserFactory(dataProvider, protocols, requestPath, version, token, device, tokenValidator), 
 					new WebSocketMessageFormatterFactory(shouldMaskResponses), 
 					new WebSocketMessageProcessorFactory(dispatcher == null ? pipeline.getServer().getDispatcher() : dispatcher), 
 					new KeepAliveDecider<WebSocketMessage>() {
